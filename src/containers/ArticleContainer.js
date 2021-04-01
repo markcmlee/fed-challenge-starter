4ea83a53790e81b54f8f27ebe91c1154ebc1864f
Mark Lee <markcmlee@gmail.com>
2021-04-01 17:41:30 -0400

    initial file structure and fake db setup

---
+++ src/containers/ArticleContainer.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Article from "../components/Article";
+
+const ArticleContainer = () => {
+  return (
+    <div>
+      <h2>Article Container</h2>
+      <Article />
+    </div>
+  );
+};
+
+export default ArticleContainer;
