1525996ce47bc5a01d951af6733373ef0284e504
Mark Lee <markcmlee@gmail.com>
2021-04-02 18:32:08 -0400

    Updates

---
+++ precache-manifest.7fb3ffb5fb7a2e4eb12cf1f1f6bec72b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d719fd0993a7387b984bfb67d89bd80",
+    "url": "/fed-challenge-starter/index.html"
+  },
+  {
+    "revision": "0161d821652ec34036bd",
+    "url": "/fed-challenge-starter/static/css/main.c6a74a23.chunk.css"
+  },
+  {
+    "revision": "36040bd2463c29773040",
+    "url": "/fed-challenge-starter/static/js/2.91dd9dcd.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/fed-challenge-starter/static/js/2.91dd9dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0161d821652ec34036bd",
+    "url": "/fed-challenge-starter/static/js/main.d341c633.chunk.js"
+  },
+  {
+    "revision": "ddf4e5f1c600b5f0db56",
+    "url": "/fed-challenge-starter/static/js/runtime-main.3972a04a.js"
+  },
+  {
+    "revision": "1542c1188204879edee18cf6e115d934",
+    "url": "/fed-challenge-starter/static/media/20-minutes-to-toned-thumb.1542c118.jpg"
+  },
+  {
+    "revision": "003901761c4b2f91b5c1b144747d4e83",
+    "url": "/fed-challenge-starter/static/media/charles-race-thumb.00390176.jpg"
+  },
+  {
+    "revision": "578dacbc0d22c3d6ff585347dadb0e73",
+    "url": "/fed-challenge-starter/static/media/full-body-hiit-thumb.578dacbc.jpg"
+  },
+  {
+    "revision": "a55f946a95a88b198f5fb3f1173a95f6",
+    "url": "/fed-challenge-starter/static/media/kafue-river-thumb.a55f946a.jpg"
+  },
+  {
+    "revision": "e0a7c9061a0e72bed419a8b9eaf2b286",
+    "url": "/fed-challenge-starter/static/media/lake-inniscarra-thumb.e0a7c906.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/fed-challenge-starter/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3e82c42342a27af7cce1e89930288333",
+    "url": "/fed-challenge-starter/static/media/performance-series-thumb.3e82c423.jpg"
+  },
+  {
+    "revision": "04edd5427547b03287f6d75cfabea837",
+    "url": "/fed-challenge-starter/static/media/shred-and-burn-thumb.04edd542.jpg"
+  },
+  {
+    "revision": "7cae9ed133157efe82011594ed819bcc",
+    "url": "/fed-challenge-starter/static/media/slow-pulls-thumb.7cae9ed1.jpg"
+  }
+]);
